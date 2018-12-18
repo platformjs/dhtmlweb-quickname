2f7b844e1b9868d8f87cd812f201a4bac5e2ddc2
Qijun Yang <Qijun.Yang@morningstar.com>
2018-12-18 01:36:01 -0600

    first commit

---
+++ dist/quickname.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nfunction isEmpty(obj) {\n    for (let p in obj) {\n        return false;\n    }\n    return true;\n}\nfunction namespace(obj, result, space) {\n    if (result.length === 0) {\n        result.push(\"{\");\n    }\n    const newSpace = space + \"  \";\n    for (let p in obj) {\n        if (typeof obj[p] === \"string\") {\n            result.push(newSpace + p + \",\");\n        } else {\n            result.push(newSpace + p + \": {\");\n            namespace(obj[p], result, newSpace + \"  \");\n            result[result.length - 1] += \",\";\n        }\n    }\n    result.push(space + \"}\");\n}\n\nfunction quickname(paths, excludePrefix) {\n    const obj = {};\n    paths.forEach(path => {\n        path = path.substr(excludePrefix.length || 0);\n        const pathNames = path.split(\"/\");\n\n        const name = pathNames[pathNames.length - 1].split(\".\")[0];\n        s += `import ${name} from \"${path}\";\\n`;\n\n        pathNames.pop();\n        let current = obj;\n        pathNames.forEach(p => {\n            if (!current[p]) {\n                current[p] = {};\n            }\n            current = current[p];\n        });\n        current[name] = name;\n    });\n    const result = [];\n    return namespace(obj, result, \"\");\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (quickname);\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ });
---
+++ index.js
@@ -0,0 +1,51 @@
+function isEmpty(obj) {
+    for(let p in obj) {
+        return false;
+    }
+    return true;
+}
+function namespace(obj, result, space) {
+    if (result.length === 0) {
+        result.push("{");
+    }
+    const newSpace = space + "  ";
+    for (let p in obj) {
+        if (typeof obj[p] === "string") {
+            result.push(newSpace + p + ",");
+        } else {
+            result.push(newSpace + p + ": {");
+            namespace(obj[p], result, newSpace + "  ");
+            result[result.length - 1] += ",";
+        }
+    }
+    result.push(space + "}");
+}
+
+function quickname(paths, excludePrefix) {
+    const obj = {};
+    let importStr = [];
+    paths.forEach(path => {
+        let pathNames = path.split("/");
+        //get rid of .js at the end
+        const name = pathNames[pathNames.length - 1].split(".")[0];
+        importStr.push(`import ${name} from "${path}";`);
+        
+
+        path = path.substr(excludePrefix.length || 0);
+        pathNames = path.split("/");
+        pathNames.pop();
+        let current = obj;
+        pathNames.forEach(p => {
+            if (!current[p]) {
+                current[p] = {};
+            }
+            current = current[p];
+        });
+        current[name] = name;
+    });
+    const result = [];
+    namespace(obj, result, "");
+    return importStr.join("\n") + "\nexport default\n " + result.join("\n");
+}
+
+module.exports = quickname;
